41048693d1716445d62aeddcf72728208bcb7987
MeiirimZh <zh.meyirim@gmail.com>
2025-02-15 18:54:48 +0500

    added check grid

---
+++ scripts/utils.py
@@ -0,0 +1,27 @@
+def check_grid(x, y, grid):
+    # Check a row
+    if set(grid[x]) == {'x'}:
+        return "x"
+    elif set(grid[x]) == {'o'}:
+        return "o"
+    
+    # Check a column
+    column_items = [row[y] for row in grid]
+    if set(column_items) == {'x'}:
+        return "x"
+    elif set(column_items) == {'o'}:
+        return "o"
+    
+    # Check all diagonals
+    main_diagonal = [grid[0][0], grid[1][1], grid[2][2]]
+    if set(main_diagonal) == {'x'}:
+        return "x"
+    elif set(main_diagonal) == {'o'}:
+        return "o"
+    secondary_diagonal = [grid[0][2], grid[1][1], grid[2][0]]
+    if set(secondary_diagonal) == {'x'}:
+        return "x"
+    elif set(secondary_diagonal) == {'o'}:
+        return "o"
+
+    return 0 
